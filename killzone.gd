1d712fbd331ffb2b03bf5a540afa19e9ade01dce
Jenny Cao <jennycamcao@gmail.com>
2026-09-13 10:23:38 -0700

    Add notes and respawn

---
+++ killzone.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body.name == "Player":
+		body.respawn()
--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 900.0
 const JUMP_VELOCITY = -900.0
 
+func respawn():
+	
+	self.global_position = Vector2(0, 0)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
